47f53a531841d5a8a52a19ab38172c84384c6fe5
hakkeem1997 <79262803+hakkeem1997@users.noreply.github.com>
2021-03-12 15:45:21 +0530

    Update product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,13 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  price = 200;
+  price =100;
+  price1=200;
+  price2=100;
   constructor() { }
   count = 0;
   count1 = 0;
   a:any;
+  b:any;
+  tot:any;
+
   ngOnInit(): void {
   }
+  pros:any=[];
   onAdd(){
     this.count++;
   }
@@ -24,4 +30,24 @@ export class ProductComponent implements OnInit {
   sum(){
      this.a=this.count * this.price;
     return this.a;
-}}
+}
+onAdd1(){
+  this.count1++;
+}
+
+onSub1(){
+  this.count1=1;
+  this.count1--;
+}
+sum1(){
+  this.b=this.count1 * this.price;
+ return this.b;
+}
+
+
+ss(){
+this. tot= this.a+this.b;
+return this.tot;
+  
+}
+}
